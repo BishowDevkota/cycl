b54feb41487bc404d067057907322b10c5318df8
Dipta Basnet <diptabasnet100@gmail.com>
2026-04-29 10:18:53 +0545

    effects added

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -8,11 +8,7 @@ import { getAboutCompanyInfo } from '@/services/about-company-info-service';
 import NewsAndNotices from '@/components/NewsAndNotices';
 import ContactHome from '@/components/ContactHome';
 import { MessageFromCeoSection } from '@/components/public/MessageFromCeoSection';
-
 import { getContactDetails } from '@/services/contact-service';
-
-
-
 
 export default async function Home() {
   const aboutCompanyInfo = await getAboutCompanyInfo();
@@ -49,23 +45,17 @@ export default async function Home() {
     <div className="flex flex-col min-h-screen">
       <HeroSection />
       <WelcomeSection aboutCompanyInfo={aboutCompanyInfo} />
-
-      <main className="relative flex-1 w-full overflow-hidden bg-[linear-gradient(180deg,#f7fafc_0%,#ffffff_45%,#f6f8fb_100%)] pt-12 pb-20 sm:pt-16 sm:pb-24 lg:pt-20 lg:pb-32">
-        <div className="pointer-events-none absolute -top-14 left-1/2 h-56 w-[65vw] -translate-x-1/2 rounded-full bg-[#8ec5b9]/20 blur-3xl" />
-        <div className="pointer-events-none absolute bottom-6 -right-24 h-40 w-40 rounded-full bg-[#f3c98f]/25 blur-3xl" />
-
+      <main className="flex-1 w-full pt-12 pb-20 sm:pt-16 sm:pb-24 lg:pt-20 lg:pb-32">
         <MessageFromCeoSection
           messageFromCeo={messageFromCeo}
           buttonLabel="Full Message"
           buttonHref="/message-from-ceo"
         />
-
         <CompanyStatsSection />
         <ServicesSection />
         <NewsAndNotices />
         <ContactHome contactDetails={contactDetails} />
       </main>
-
       <Footer />
     </div>
   );
