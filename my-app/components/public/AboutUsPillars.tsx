57b41a08de55f296ffac790237d0b791f39848a5
Devashish Upreti <devashup68@gmail.com>
2026-04-30 19:01:55 +0545

    improved ui of the aboutuspillars

--- my-app/components/public/AboutUsPillars.tsx
+++ my-app/components/public/AboutUsPillars.tsx
@@ -38,13 +38,13 @@ export function AboutUsPillars({
         {cards.map((card) => (
           <article
             key={card.title}
-            className="flex h-full flex-col bg-[#016f81] p-8 text-white shadow-[0_12px_28px_rgba(0,0,0,0.12)] duration-300 ease-out transform hover:-translate-y-1"
+            className="flex h-full flex-col bg-white  text-teal-deep shadow-[0_12px_28px_rgba(0,0,0,0.12)] duration-300 ease-out transform hover:-translate-y-1"
           >
-            <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center justify-between mb-2 bg-teal-deep text-white px-6 py-4">
               <h3 className="text-[24px] font-bold uppercase tracking-[4px]">
                 {card.title}
               </h3>
-              <div className="h-10 w-10 rounded-full bg-white flex items-center justify-center">
+              <div className="h-10 w-10 rounded-full flex items-center justify-center bg-white">
                <Image
                   src={`${card.imageUrl}`}
                   alt={`${card.title} icon`} height={80} width={80} />
@@ -52,18 +52,14 @@ export function AboutUsPillars({
             </div>
 
             {card.content ? (
-              <p className="mt-4 text-base leading-6 text-white/90">
+              <p className="mt-2 ext-base leading-6 text-teal-deep px-6 pb-4">
                 {card.content}
               </p>
             ) : (
-              <ul className="mt-4 space-y-2 text-base leading-6 text-white/90">
+              <ul className="mt-2 space-y-2 text-base leading-6 w-full px-6 pb-4 list-disc list-inside marker:text-teal-deep">
                 {card.items?.map((item) => (
-                  <li key={item} className="flex gap-3">
-                    <span
-                      className="mt-2 inline-flex h-2 w-2 shrink-0 rounded-full bg-white/80"
-                      aria-hidden="true"
-                    />
-                    <span>{item}</span>
+                  <li key={item} className="pl-1">
+                    {item}
                   </li>
                 ))}
               </ul>
